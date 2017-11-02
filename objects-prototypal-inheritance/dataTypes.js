896ae8764a63cd306c471181012ab29554446417
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2017-11-02 18:53:00 -0300

    Object-prototypal

---
+++ objects-prototypal-inheritance/dataTypes.js
@@ -0,0 +1,9 @@
+
+
+const person = 'Kobe';
+
+const anotherPerson = person // anotherPerson = the value of person​
+person = 'Bryant';
+​
+console.log(anotherPerson); // Kobe​
+console.log(person); // Bryant
---
+++ objects-prototypal-inheritance/object-constructor.js
@@ -0,0 +1,7 @@
+const orange = new Object()
+orange.color = "orange"
+orange.shape = "round"
+orange.sweetness = 4​
+orange.howSweetAmI = function () {
+    console.log("Hmm Hmm Good")
+}
